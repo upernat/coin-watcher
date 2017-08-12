f5ef3cf2dd3b99fe05a61d77d48fb7385336f879
Uros Pernat <upernat@gmail.com>
2017-08-12 11:58:11 +0200

    Jax RS config.

---
+++ src/main/java/coin/market/cap/business/facade/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package coin.market.cap.business.facade;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Created on 12/08/17.
+ *
+ * @author urospernat
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+}
